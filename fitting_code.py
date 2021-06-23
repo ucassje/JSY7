643acce8e5c3a958b04a11d45f7464fff6b47c16
seong-yeop <ucassje@ucl.ac.uk>
2021-06-23 07:46:04 +0100

    update

--- fitting_code.py
+++ fitting_code.py
@@ -145,7 +145,7 @@ for r in range(Nr):
     print(r)
     if r==0:
             p = lmfit.Parameters()
-            p.add_many(('nc', 1,True,0.98,1),('ns', 0,True,0,0.02), ('Tc_pal', 10*10**5,True,9*10**5,12*10**5), ('Tc_per', 10*10**5,True,9*10**5,12*10**5), ('Ts_pal', 10*10**5,True,1*10**5,20*10**5), ('Ts_per', 10*10**5,True,1*10**5,20*10**5), ('Uc',0,True,-1.5,0),('Us',0,True,0,0.5))
+            p.add_many(('nc', 1,True,0.8,1),('ns', 0,True,0,0.2), ('Tc_pal', 10*10**5,True,1*10**5,12*10**5), ('Tc_per', 10*10**5,True,1*10**5,12*10**5), ('Ts_pal', 10*10**5,True,1*10**5,20*10**5), ('Ts_per', 10*10**5,True,1*10**5,20*10**5), ('Uc',0,True,-1.5,0),('Us',0,True,0,0.5))
     else:                          #,('Us',0,True,0,1.5) , ('Uc',0,True,-0.4,0) 
             p = lmfit.Parameters()
             p.add_many(('nc', nc[r-1],True,0.8,1),('ns', ns[r-1],True,0,0.2), ('Tc_pal', Tc_pal[r-1],True,1*10**5,10*10**5), ('Tc_per', Tc_per[r-1],True,1*10**5,10*10**5), ('Ts_pal', Ts_pal[r-1],True,1*10**5,30*10**5), ('Ts_per', Ts_per[r-1],True,1*10**5,20*10**5), ('Uc',Uc[r-1],True,-1.5,0),('Us',Us[r-1],True,0,17))
