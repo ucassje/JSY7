cca66a91c76ed6dc823b79c05fc39670f85f0df6
seong-yeop <ucassje@ucl.ac.uk>
2021-07-02 16:33:44 +0100

    update

--- EulerCN2.py
+++ EulerCN2.py
@@ -938,15 +938,7 @@ for p in range(updatetime):
                 f_1[:,0]=f_initial[:,0]
 
 
-                f_temp4=np.zeros(shape = (Nv**2, Nr))
-                f_temp4[:,:]=f_1[:,:]                                
-                for r in range(Nr-1):
-                            for j in range(Nv):
-                                    for i in range(Nv):
-                                            if i!=Nv-1 and i!=0 and j!=Nv-1 and j!=0:
-                                                    f_temp4[j*Nv+i,r+1]=(1/4)*(2*f_1[j*Nv+i,r+1]+0.5*f_1[j*Nv+i+1,r+1]*(f_pre[j*Nv+i,r+1]/f_pre[j*Nv+i+1,r+1])+0.5*f_1[j*Nv+i-1,r+1]*(f_pre[j*Nv+i,r+1]/f_pre[j*Nv+i-1,r+1])+0.5*f_1[(j+1)*Nv+i,r+1]*(f_pre[(j)*Nv+i,r+1]/f_pre[(j+1)*Nv+i,r+1])+0.5*f_1[(j-1)*Nv+i,r+1]*(f_pre[(j)*Nv+i,r+1]/f_pre[(j-1)*Nv+i,r+1]))                             
-                f_1[:,:]=f_temp4[:,:]
-                f_1[:,0]=f_initial[:,0]
+                
 
 
                 f_temp1=np.zeros(shape = (Nv**2, Nr))
