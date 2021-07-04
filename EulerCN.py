2b836063840e9558f9937099721415a9a46adced
seong-yeop <ucassje@ucl.ac.uk>
2021-07-04 22:18:08 +0100

    update

--- EulerCN.py
+++ EulerCN.py
@@ -627,7 +627,7 @@ def Matrix_QQ(R):
 
 
 f_1 = np.load('data_next.npy')
-updatetime=2
+updatetime=1
 timestep=100 #700
 Normvalue=np.zeros(shape = (timestep*updatetime))
 for p in range(updatetime):
