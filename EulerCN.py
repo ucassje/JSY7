f376cf577efdec5b2e66201282ad29f547ae11bc
seong-yeop <ucassje@ucl.ac.uk>
2021-07-07 20:38:31 +0100

    update

--- EulerCN.py
+++ EulerCN.py
@@ -1235,7 +1235,7 @@ for p in range(updatetime):
                 for r in range(Nr-2):
                             for j in range(Nv):
                                     for i in range(Nv):
-                                                f_temp4[j*Nv+i,r+1]=0.5*(0.5*(f_1[j*Nv+i,r]*(f_pre[j*Nv+i,r+1]/f_pre[j*Nv+i,r])+f_1[j*Nv+i,r+1])+0.5*(f_1[j*Nv+i,r+1]+f_1[j*Nv+i,r+2]*(f_pre[j*Nv+i,r+1]/f_pre[j*Nv+i,r+2]))) #0.5*(0.5*(f_1[j*Nv+i,r]*ratio_r[j*Nv+i,r]**(-1)+f_1[j*Nv+i,r+1])+0.5*(f_1[j*Nv+i,r+1]+f_1[j*Nv+i,r+2]*ratio_r[j*Nv+i,r+1]))     #0.5*(f_1[(r)*(Nv)*(Nv)+j*Nv+i]*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)+f_1[(r+2)*(Nv)*(Nv)+j*Nv+i]*ratio_r[(r+1)*(Nv)*(Nv)+j*Nv+i])                                
+                                                f_temp4[j*Nv+i,r+1]=0.5*(0.5*(f_1[j*Nv+i,r]*ratio_r[j*Nv+i,r]**(-1)+f_1[j*Nv+i,r+1])+0.5*(f_1[j*Nv+i,r+1]+f_1[j*Nv+i,r+2]*ratio_r[j*Nv+i,r+1]))     #0.5*(f_1[(r)*(Nv)*(Nv)+j*Nv+i]*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)+f_1[(r+2)*(Nv)*(Nv)+j*Nv+i]*ratio_r[(r+1)*(Nv)*(Nv)+j*Nv+i])                                
                 f_1[:,:]=f_temp4[:,:]
                 f_1[:,0]=f_initial[:,0]
 
