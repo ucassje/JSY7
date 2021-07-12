8f6a8822687d42505fa790544e6b1ffb56106c58
seong-yeop <ucassje@ucl.ac.uk>
2021-07-12 19:48:39 +0100

    update

--- EulerCN.py
+++ EulerCN.py
@@ -1246,23 +1246,23 @@ for p in range(updatetime):
                                                         f_1[j*Nv+i,r]=f_pre[j*Nv+i,r]
 
 
-                #f_1[:,Nr-1]=f_1[:,Nr-2]*ratio_r[:,Nr-2]**(-1)
-                f_temp4=np.zeros(shape = (Nv**2, Nr))
-                f_temp4[:,:]=f_1[:,:]                                
-                for r in range(Nr-1):
-                            for j in range(Nv):
-                                    for i in range(Nv):
-                                            if r==Nr-2:
-                                                    f_temp4[j*Nv+i,r+1]=f_1[j*Nv+i,r]*ratio_r[j*Nv+i,r]**(-1)#2*f_temp4[(r)*(Nv)*(Nv)+(j)*Nv+i]*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)-f_temp4[(r-1)*(Nv)*(Nv)+(j)*Nv+i]*ratio_r[(r-1)*(Nv)*(Nv)+j*Nv+i]**(-1)*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)   
-                f_1[:,:]=f_temp4[:,:]
-                f_1[:,0]=f_initial[:,0]
+                f_1[:,Nr-1]=f_1[:,Nr-2]*ratio_r[:,Nr-2]**(-1)
+                #f_temp4=np.zeros(shape = (Nv**2, Nr))
+                #f_temp4[:,:]=f_1[:,:]                                
+                #for r in range(Nr-1):
+                #            for j in range(Nv):
+                #                    for i in range(Nv):
+                #                            if r==Nr-2:
+                #                                    f_temp4[j*Nv+i,r+1]=f_1[j*Nv+i,r]*ratio_r[j*Nv+i,r]**(-1)#2*f_temp4[(r)*(Nv)*(Nv)+(j)*Nv+i]*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)-f_temp4[(r-1)*(Nv)*(Nv)+(j)*Nv+i]*ratio_r[(r-1)*(Nv)*(Nv)+j*Nv+i]**(-1)*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)   
+                #f_1[:,:]=f_temp4[:,:]
+                #f_1[:,0]=f_initial[:,0]
 
                 f_temp4=np.zeros(shape = (Nv**2, Nr))
                 f_temp4[:,:]=f_1[:,:]                                
                 for r in range(Nr-2):
                             for j in range(Nv):
                                     for i in range(Nv):
-                                                f_temp4[j*Nv+i,r+1]=0.5*(0.5*(f_1[j*Nv+i,r]*ratio_r[j*Nv+i,r]**(-1)+f_1[j*Nv+i,r+1])+0.5*(f_1[j*Nv+i,r+1]+f_1[j*Nv+i,r+2]*ratio_r[j*Nv+i,r+1]))     #0.5*(f_1[(r)*(Nv)*(Nv)+j*Nv+i]*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)+f_1[(r+2)*(Nv)*(Nv)+j*Nv+i]*ratio_r[(r+1)*(Nv)*(Nv)+j*Nv+i])                                
+                                                f_temp4[j*Nv+i,r+1]=0.5*(0.5*(f_1[j*Nv+i,r]*(f_pre[j*Nv+i,r+1]/f_pre[j*Nv+i,r])+f_1[j*Nv+i,r+1])+0.5*(f_1[j*Nv+i,r+1]+f_1[j*Nv+i,r+2]*(f_pre[j*Nv+i,r+1]/f_pre[j*Nv+i,r+2])))#0.5*(0.5*(f_1[j*Nv+i,r]*ratio_r[j*Nv+i,r]**(-1)+f_1[j*Nv+i,r+1])+0.5*(f_1[j*Nv+i,r+1]+f_1[j*Nv+i,r+2]*ratio_r[j*Nv+i,r+1]))     #0.5*(f_1[(r)*(Nv)*(Nv)+j*Nv+i]*ratio_r[r*(Nv)*(Nv)+j*Nv+i]**(-1)+f_1[(r+2)*(Nv)*(Nv)+j*Nv+i]*ratio_r[(r+1)*(Nv)*(Nv)+j*Nv+i])                                
                 f_1[:,:]=f_temp4[:,:]
                 f_1[:,0]=f_initial[:,0]
 
