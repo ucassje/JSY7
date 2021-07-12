af5c9a7421c2d00c5ea2902f689423952d09b8f4
seong-yeop <ucassje@ucl.ac.uk>
2021-07-12 18:25:47 +0100

    update

--- restrictvelocityfitting.py
+++ restrictvelocityfitting.py
@@ -147,7 +147,7 @@ for r in range(Nr):
    Density[r]=tempDensity/(r_s**3)
 
 for j in range(Nv):
-        if per_v[j]>16:
+        if per_v[j]>15:
             h=h+1
                 
 f_11=np.zeros(shape = (Nv*(Nv-2*h), 1))
