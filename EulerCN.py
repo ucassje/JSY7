692fb0858c41b8f4642a76f76adad8bcd0d6d5be
seong-yeop <ucassje@ucl.ac.uk>
2021-07-12 19:06:04 +0100

    update

--- EulerCN.py
+++ EulerCN.py
@@ -1390,7 +1390,7 @@ for r in range(Nr):
 
 for r in range(Nr):
    for i in range(Nv):
-        solu2[i]=np.log10(f_1[(40)*Nv+i,r]/np.amax(f_1))
+        solu2[i]=np.log10(f_1[(15)*Nv+i,r]/np.amax(f_1))
    fig = plt.figure()
    fig.set_dpi(500)
    plt.plot(pal_v,solu2,color='k',label=r'$r/r_s=$' "%.2f" % z[r]);
@@ -1415,7 +1415,7 @@ for r in range(Nr):
 
 for r in range(Nr):
    for j in range(Nv):
-        solu4[j]=np.log10(f_1[(j)*Nv+40,r]/np.amax(f_1))
+        solu4[j]=np.log10(f_1[(j)*Nv+15,r]/np.amax(f_1))
    fig = plt.figure()
    fig.set_dpi(500)
    plt.plot(per_v,solu4,color='k',label=r'$r/r_s=$' "%.2f" % z[r]);
